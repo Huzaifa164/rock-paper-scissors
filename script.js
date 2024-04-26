bef698b26bcd20b6bfaa99aa36538be3278c703d
Huzaifa164 <ansarihuzaifa654321@gmail.com>
2024-04-26 12:10:23 +0530

    Added UI

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const computerDecisons = ['rock', 'paper', 'scissor'];
 let computerPoints = 0;
 let playerPoints = 0;
-
+const btnContainer = document.querySelector('#btn-container');
+const message = document.querySelector("#message");
+const score = document.querySelector('#score');
+const finalMessage = document.querySelector("#final-message");
+const resetMessage = document.querySelector("#reset-message");
+let counter = 0;
 
 function getComputerChoice() {
     const decisionIndex = Math.floor(Math.random() * (3));
@@ -9,7 +14,11 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    counter++;
     if(playerSelection.toLowerCase() == computerSelection) {
+        playerPoints++;
+        score.textContent = playerPoints;
+        computerPoints++;
         return 'Game Tied!';
     } else if (playerSelection.toLowerCase() == 'rock') {
         if(computerSelection == 'paper') {
@@ -36,6 +45,7 @@ function playRound(playerSelection, computerSelection) {
 
 function winHelper(playerSelection, computerSelection) {
     playerPoints++;
+    score.textContent = playerPoints;
     return 'You won! ' + playerSelection + ' beats ' + computerSelection;
 }
 
@@ -44,16 +54,33 @@ function loseHelper(playerSelection, computerSelection) {
     return 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
 }
 
-for(let i = 1; i <= 5; i++) {
-    computerSelection = getComputerChoice();
-    playerSelection = prompt('Please Enter your Decision: ');
-    console.log(playRound(playerSelection, computerSelection));
-}
+// for(let i = 1; i <= 5; i++) {
+//     computerSelection = getComputerChoice();
+//     playerSelection = prompt('Please Enter your Decision: ');
+//     console.log(playRound(playerSelection, computerSelection));
+// }
 
-if(computerPoints == playerPoints) {
-    console.log('Game Tied!');
-} else if (computerPoints > playerPoints) {
-    console.log('Computer Won!');
-} else {
-    console.log('You Won!');
-}
+btnContainer.addEventListener('click', (event) => {
+    if(event.target.id == "reset") {
+        window.location.reload();
+    }
+    if(counter > 5) {
+        resetMessage.textContent = 'Please Press Reset Button to play more.';
+        return;
+    }
+    let computerSelection = getComputerChoice();
+    message.textContent = playRound(event.target.id, computerSelection);
+    if(counter == 5) {
+        console.log("Computer: " + computerPoints);
+        console.log("Player: " + playerPoints);
+        if(computerPoints == playerPoints) {
+            finalMessage.textContent = "Game Tied!";
+        } else if (computerPoints > playerPoints) {
+            finalMessage.textContent = "You Lose!";
+        } else {
+            finalMessage.textContent = "You Won!";
+        }
+        counter++;
+    }
+})
+
